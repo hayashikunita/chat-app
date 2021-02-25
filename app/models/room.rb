fa91307e0f2a489d3171c158124dd647cc1bf04b
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-02-25 12:19:42 +0900

    Revert "不明"
    This reverts commit 13938750b5fb10f0081496028a075a96ab6c5d5d.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
-<<<<<<< Updated upstream
 
   validates :name, presence: true
 
 end
-=======
-  has_many :messages
-
-  validates :name, presence: true
-end
->>>>>>> Stashed changes
